e102124f1e87487c03c15b3c97f64eb5498e355b
BlckTitan <dorshman406@gmail.com>
2022-12-22 18:25:37 +0100

    modified the navigation bar

--- src/Current/CurrentWeather.js
+++ src/Current/CurrentWeather.js
@@ -1,7 +1,37 @@
-import React from 'react'
+import React from 'react';
+
 
 export default function CurrentWeather() {
   return (
     <div>CurrentWeather</div>
   )
 }
+/**cloudy image
+https://images.unsplash.com/photo-1523556329929-93033da89632?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80
+https://images.unsplash.com/photo-1509803874385-db7c23652552?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80
+https://images.unsplash.com/photo-1514454529242-9e4677563e7b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80
+*/
+/**
+ * sunny sky 
+ * https://images.unsplash.com/photo-1525490829609-d166ddb58678?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80
+   https://images.unsplash.com/photo-1504386106331-3e4e71712b38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80
+   https://images.unsplash.com/photo-1616254717885-62fc52b4ef38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80
+*/
+
+/**rainy day
+ * https://images.unsplash.com/photo-1501999635878-71cb5379c2d8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1631&q=80
+ * https://images.unsplash.com/photo-1610741083696-4aa5185062ef?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80
+ */
+
+/**
+ * thunder
+ * https://images.unsplash.com/photo-1643119222678-131a90659e0e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80
+ */
+
+/**
+ * stormy day
+ * https://images.unsplash.com/photo-1527482937786-6608f6e14c15?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80
+ * https://images.unsplash.com/photo-1600377927594-ceae8f8981a6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80
+ * https://images.unsplash.com/photo-1530131888162-03372d8eeff0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80
+ * 
+ */
